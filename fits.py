4c0ac2bfe4e247215aa8ce8eb3cddeec2e0d3aef
Robert Nikutta <robert@pa.uky.edu>
2016-08-03 22:10:40 -0400

    CDS class updated, CDS fitting func written, plotting func generalized.

--- fits.py
+++ fits.py
@@ -115,7 +115,16 @@ def get_stats(tracesdict,chi2redtrace,printtable=False):
     return bestvals, quantiles
 
 
-def plot_MAP_posteriors_kde(data,sig,tracesdict,chi2trace):
+def plot_MAP_posteriors_kde(data,sig,tracesdict,chi2trace,vars,xlabels,modeltype='ConstantDensityShell'):
+
+    """Plot data, MAP model, marginalized posterior, mean radial profiles, and compute best model cube.
+
+    Parameters:
+    -----------
+
+    data,sig,tracesdict,chi2trace : As provided by fit_* function
+
+    """
 
     import pandas as pd
     import seaborn as sb
@@ -140,22 +149,26 @@ def plot_MAP_posteriors_kde(data,sig,tracesdict,chi2trace):
     print "medians = ", medians
 
     
-    #    image2d_best, cube_best, scale_best, theta_best = S61_get_best_model(data,data2d,sig2d,tracesdict,chi2trace)
-    mod = models.TruncatedNormalShell(data.npix,transform=Quad())
+    modinstance = getattr(models,modeltype)
+    mod = modinstance(data.npix,transform=Quad())  # CDS
     image2d_best_map, cube_best_map, scale_best_map, theta_best_map = S61_get_best_model(data,sig,tracesdict,chi2trace,mod,theta=theta_map[:-1]+[1,1])
 
-    vars = ['r','width','clipa','clipb','Mion']
-    xlabels = [r'radius (pc)',r'thickness (pc)',r'lower clip radius (pc)',r'upper clip radius (pc)',r'M$_{\rm ion}$ (M$_{\odot}$)']
+    
+##TNS    vars = ['r','width','clipa','clipb','Mion']
+#    vars = ['rin','rout','Mion']  # CDS
+##TNS    xlabels = [r'radius (pc)',r'width (pc)',r'lower clip radius (pc)',r'upper clip radius (pc)',r'M$_{\rm ion}$ (M$_{\odot}$)']
+#    xlabels = [r'inner radius (pc)',r'outer radius (pc)',r'M$_{\rm ion}$ (M$_{\odot}$)']  #CDS
 
     fontsize = 12
     p.rcParams['legend.fontsize'] = fontsize
     p.rcParams['axes.labelsize'] = fontsize
     p.rcParams['axes.titlesize'] = fontsize
     p.rcParams['text.fontsize'] =  fontsize-1
-    p.rcParams['legend.fontsize'] = fontsize
+    p.rcParams['legend.fontsize'] = fontsize-2
     p.rcParams['xtick.labelsize'] = fontsize
     p.rcParams['ytick.labelsize'] = fontsize
-    p.rcParams['font.family'] = 'sans-serif'
+#    p.rcParams['font.family'] = 'sans-serif'
+    p.rcParams['font.family'] = 'serif'
 #latex    # don't use Type 3 fonts (requirement by MNRAS); you'll need dvipng installed
 #latex    p.rcParams['ps.useafm'] = True
 #latex    p.rcParams['pdf.use14corefonts'] = True
@@ -166,7 +179,10 @@ def plot_MAP_posteriors_kde(data,sig,tracesdict,chi2trace):
 #latex        r'\sansmath'               # <- tricky! -- gotta actually tell tex to use!
 #latex    ] 
 
-    f, axes = p.subplots(2, 4, figsize=(12, 6), sharex=False, sharey=False)#,subplot_kw={'aspect':'auto'})
+#TNS    f, axes = p.subplots(2, 4, figsize=(12, 6), sharex=False, sharey=False)
+    ncols = int(N.ceil((len(vars)+3)/2.))
+#    f, axes = p.subplots(2, 3, figsize=(10, 6), sharex=False, sharey=False)
+    f, axes = p.subplots(2, ncols, figsize=(3*ncols, 2*3), sharex=False, sharey=False)
     lw = 2.
     al = 0.4
 
@@ -178,7 +194,7 @@ def plot_MAP_posteriors_kde(data,sig,tracesdict,chi2trace):
     extent = [ticks[0],ticks[-1],ticks[0],ticks[-1]]
 
     ax00 = axes[0,0]
-    ax00.imshow(data2d,origin='lower',extent=extent,cmap=cmap,interpolation='none',norm=mpl.colors.Normalize(data2d.min(),data2d.max()))
+    ax00.imshow(data2d,origin='lower',extent=extent,cmap=cmap,interpolation='none')
     ax00.text(0.04,0.9,'DATA',ha='left',transform=ax00.transAxes,color='k')
     ax00.text(0.97,0.9,'(0)',ha='right',transform=ax00.transAxes,color='k')
     ax00.set_xlabel('x (pc)')
@@ -191,22 +207,35 @@ def plot_MAP_posteriors_kde(data,sig,tracesdict,chi2trace):
     
     # IMAGE MAP MODEL
     ax10 = axes[1,0]
-    ax10.imshow(image2d_best_map,origin='lower',extent=extent,cmap=cmap,interpolation='none',norm=mpl.colors.Normalize(data2d.min(),data2d.max()))
-    center = (theta_best_map[4],theta_best_map[5])
-    circ1 = p.Circle(center,theta_best_map[0],ec='k',fc='none',ls='solid',lw=0.7,alpha=0.5)
-    circ2a = p.Circle(center,theta_best_map[0]+theta_best_map[1],ec='k',fc='none',ls='dashed',lw=0.7,alpha=0.5)
-    if theta_best_map[0]-theta_best_map[1] > 0:
-        circ2b = p.Circle(center,theta_best_map[0]-theta_best_map[1],ec='k',fc='none',ls='dashed',lw=0.7,alpha=0.5)
-    else:
-        circ2b = None
-        
-    circ3 = p.Circle(center,theta_best_map[3],ec='k',fc='none',ls='dotted',lw=0.7,alpha=0.5)
-    ax10.add_patch(circ1)
-    ax10.add_patch(circ2a)
-    if circ2b is not None:
-        ax10.add_patch(circ2b)
+    ax10.imshow(image2d_best_map,origin='lower',extent=extent,cmap=cmap,interpolation='none')
 
-    ax10.add_patch(circ3)
+    # TODO: automate this
+    if modeltype == 'ConstantDensityShell':
+        # use this for CDS
+        center = (theta_best_map[2],theta_best_map[3])
+        circ1 = p.Circle(center,theta_best_map[0],ec='k',fc='none',ls='solid',lw=0.7,alpha=0.5)
+        circ2 = p.Circle(center,theta_best_map[1],ec='k',fc='none',ls='dashed',lw=0.7,alpha=0.5)
+        ax10.add_patch(circ1)
+        ax10.add_patch(circ2)
+    elif modeltype == 'TruncatedNormalShell':
+        # use this for TNS
+        center = (theta_best_map[4],theta_best_map[5])
+        circ1 = p.Circle(center,theta_best_map[0],ec='k',fc='none',ls='solid',lw=0.7,alpha=0.5)
+        circ2a = p.Circle(center,theta_best_map[0]+theta_best_map[1],ec='k',fc='none',ls='dashed',lw=0.7,alpha=0.5)
+        if theta_best_map[0]-theta_best_map[1] > 0:
+            circ2b = p.Circle(center,theta_best_map[0]-theta_best_map[1],ec='k',fc='none',ls='dashed',lw=0.7,alpha=0.5)
+        else:
+            circ2b = None
+            
+        circ3 = p.Circle(center,theta_best_map[3],ec='k',fc='none',ls='dotted',lw=0.7,alpha=0.5)
+        ax10.add_patch(circ1)
+        ax10.add_patch(circ2a)
+        if circ2b is not None:
+            ax10.add_patch(circ2b)
+
+        ax10.add_patch(circ3)
+
+    
     ax10.set_xticks(ticks)
     ax10.set_xticklabels(ticklabels)
     ax10.set_yticks(ticks)
@@ -222,8 +251,11 @@ def plot_MAP_posteriors_kde(data,sig,tracesdict,chi2trace):
     ax10.set_yticklabels(ticklabels)
 
 
-    
-    axorder = [(0,1),(1,1),(0,2),(1,2),(0,3)]
+    # TODO: try to automate this
+    if modeltype == 'TruncatedNormalShell':
+        axorder = [(0,1),(1,1),(0,2),(1,2),(0,3)]
+    elif modeltype == 'ConstantDensityShell':
+        axorder = [(0,1),(1,1),(0,2)]
     
     for j,jax in enumerate(axorder):
         print j, jax
@@ -248,11 +280,12 @@ def plot_MAP_posteriors_kde(data,sig,tracesdict,chi2trace):
         dashes = [6,3,6,3] # 10 points on, 5 off, 100 on, 5 off
         line.set_dashes(dashes)
 
-        if j in (0,1):
-            ax.set_ylabel('marginalized posteriors')
-        if j == 3:
-            leg = ax.legend(loc='center',bbox_to_anchor=(1.65,0.5),frameon=True)
-            leg.legendPatch.set_alpha(0.7)
+        ax.set_ylabel('marginalized posterior')
+        if j == ncols:
+#            leg = ax.legend(loc='center',bbox_to_anchor=(1.65,0.5),frameon=True)
+            leg = ax.legend(loc='lower right',frameon=True)
+            leg.legendPatch.set_alpha(0.8)
+            leg.get_frame().set_facecolor('1.')
             leg.get_frame().set_edgecolor('0.3')
 
         
@@ -267,19 +300,82 @@ def plot_MAP_posteriors_kde(data,sig,tracesdict,chi2trace):
         ax.yaxis.set_major_locator(LinearLocator(5))
 #        ax.yaxis.set_major_formatter(FormatStrFormatter(fmt))
         ax.yaxis.set_major_formatter(NullFormatter())
-        
-    axes[-1, -1].axis('off')
-        
-    f.subplots_adjust(left=0.06,right=0.98,top=0.96,bottom=0.1,hspace=0.3,wspace=0.2)
 
-    f.savefig('S61_posteriors_MAPMEDmodels.pdf')
 
-    return cube_best_map,scale_best_map
+    # mean profiles
+    auxdata = data2d.data
+    co = N.isnan(auxdata)
+    auxdata[co] = 0.
+    yscale = 1000.
+    meanprofile_data = get_mean_profile(auxdata) / yscale
+    meanprofile_map = get_mean_profile(image2d_best_map) / yscale
+    ax = axes[-1,-1]
+    ax.plot(meanprofile_data,'b-',lw=2,alpha=0.6,label='data')
+    ax.plot(meanprofile_map,'r-',lw=2,alpha=0.6,label='MAP model')
+    ax.text(0.97,0.9,'(%d)' % (2*ncols-1),ha='right',transform=ax.transAxes,color='k')
+    ax.set_ylim(0,1.01*max(meanprofile_data.max(),meanprofile_map.max()))
+    ax.set_xlabel('radial coordinate (pc)')
+    ax.set_xticks(N.linspace(0,40,5))
+    ax.set_xticklabels(ticklabels)
+    ax.set_ylabel(r'$\rm EM\ (%d\ \times\ pc\ cm^{-6}\ px^{-1})$' % yscale)
+    ax.yaxis.set_major_locator(LinearLocator(5))
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+    leg = ax.legend(loc='lower left',frameon=1,title='Mean profiles',handlelength=1.,borderaxespad=0.1)
+    leg.legendPatch.set_alpha(0.8)
+    leg.get_frame().set_facecolor('1.')
+    leg.get_frame().set_edgecolor('0.3')
+    #    axes[-1, -1].axis('off')
+        
+    f.subplots_adjust(left=0.065,right=0.975,top=0.96,bottom=0.1,hspace=0.3,wspace=0.4)
+
+    f.savefig('S61_posteriors_MAPmodels_CDS.png',dpi=300)
+
+    return cube_best_map,scale_best_map,   meanprofile_data
+
+
+def get_profile(image,pa=0.):
+
+    """Parameters
+    ----------
+
+    image : 2d array
+    
+    pa : float
+        Position angle in degrees. 0. mean North, and positive values
+        mean counter-clockwise (i.e. in the astronomical sense) from
+        the North positions.
+
+    """
+
+    from scipy import ndimage
+
+    rotimage = ndimage.rotate(image,pa,reshape=False)
+    midcol = image.shape[0]/2
+    profile = rotimage[midcol,:]
+
+    return profile
+
+
+def get_mean_profile(image,delta=10.):
+
+    nsteps = int(180. / float(delta))
+
+    profiles = []
+    for j in xrange(nsteps-1):
+        angle = (j+1)*delta
+        profile = get_profile(image,pa=angle)
+        profiles.append(profile)
+
+    meanprofile = N.mean(profiles,axis=0)
+
+    return meanprofile
+    
 
 
 def get_Mion_trace(data,sig2d,tracesdict,mod):
 
-    n = tracesdict['r'].size
+    k = tracesdict.keys()[0]
+    n = tracesdict[k].size
     traces = tracesdict.values()
 
     Mion_trace = N.zeros(n)
@@ -496,9 +592,10 @@ def chi2(d,e,m):
 
 # FITTING FUNCTIONS
 
-def fit_S61_TNS_continuous(datafile='/home/robert/science/ownpapers/lbv-paper/data-em-maps/em_map_s61-8GHz.fits',\
-                           sigmafile='/home/robert/science/ownpapers/lbv-paper/data-em-maps/err_em_map_s61-8GHz.fits',\
-                           nsample=500,nburn=100,physical_units=True,return_Mion=True,pickle=None):
+def fit_S61_CDS(datafile='/home/robert/science/ownpapers/lbv-paper/data-em-maps/em_map_s61-8GHz.fits',\
+                sigmafile='/home/robert/science/ownpapers/lbv-paper/data-em-maps/err_em_map_s61-8GHz.fits',\
+                nsample=500,nburn=100,physical_units=True,return_Mion=True,pickle=None,\
+                modeltype='ConstantDensityShell'):
 
     # get image, flatten, normalize
     print "Reading data file"
@@ -525,9 +622,111 @@ def fit_S61_TNS_continuous(datafile='/home/robert/science/ownpapers/lbv-paper/da
              
     # ///// SET UP PRIORS
     eps = 0.001  # to avoid numerical armageddons
-    r = pymc.Uniform('r',0.2/factor,0.35/factor)  # r=mu of the Gaussian; values are in parsec, factor converts them to unit hypercube
-    width = pymc.Uniform('width',0.07/factor,(0.54/factor-r))#,value=0.05)  # width=sigma of the Gaussian
-    clipa = pymc.Uniform('clipa',0.,r-eps)
+    rin = pymc.Uniform('rin',0.05/factor,0.15/factor)  # r=mu of the Gaussian; values are in parsec, factor converts them to unit hypercube
+    rout = pymc.Uniform('rout',rin+eps,(0.45/factor))#,value=0.05)  # width=sigma of the Gaussian
+    # offsets drawn from narrow Gaussian, truncated at [-2,+2] pixels from (x,y)=(0,0)
+    xoff = pymc.TruncatedNormal('xoff',0,1./0.02**2,-0.02,0.02)
+    yoff = pymc.TruncatedNormal('yoff',0,1./0.02**2,-0.02,0.02)
+    # ///// END OF SETTING UP PRIORS
+
+    # model: truncated Gaussian shell
+#    mod = models.ConstantDensityShell(npix,transform=Quad())  # Quad() will square ne before integration
+
+    modinstance = getattr(models,modeltype)
+    mod = modinstance(data.npix,transform=Quad())  # CDS
+    
+    # MCMC sampling happens inside here
+    @pymc.deterministic()
+    def modeled_data(rin=rin,rout=rout,xoff=xoff,yoff=yoff):
+
+        mod(rin,rout,xoff,yoff,1,1.)
+        image = N.sum(mod.transform(mod.rho),axis=-1)
+        image2d, dummy, scale = get_scale(image,data2d,sig2d, returnall=True)
+
+        return image2d
+   
+    
+    # MCMC model
+    modely = pymc.Normal('modely',mu=modeled_data,tau=1./sig2d**2,value=data2d,observed=True)
+    model = pymc.Model([rin,rout,xoff,yoff,modely])
+    M = pymc.MCMC(model)
+
+    
+    # adaptive stepping methods, can lead to better convergence of MCMC chains
+    for par in (rin,rout,xoff,yoff):
+        M.use_step_method(pymc.AdaptiveMetropolis,[par],delay=100,shrink_if_necessary=True,greedy=False)
+
+    # run the MCMC sampling
+    M.sample(iter=nsample,burn=nburn,thin=1)
+
+    # convenience
+    from collections import OrderedDict
+    params = ('rin','rout','xoff','yoff')
+    tracesdict = OrderedDict([(par,M.trace(par).gettrace()) for par in params])
+
+    chi2red_trace, idx, chi2red_min = get_logp_trace(data2d,sig2d,tracesdict,mod)
+
+    if return_Mion is True:
+        print "Computing Mion"
+        Mion_trace = get_Mion_trace(data,sig2d,tracesdict,mod)
+        tracesdict['Mion'] = Mion_trace
+
+#    if physical_units is True:
+    if physical_units is not None:
+        print "Converting traces to physical units"
+        for k,v in tracesdict.items():
+            if k in params:
+                print k
+                v = v * factor  # now in pc
+                tracesdict[k] = v
+
+    # pickling means to "save a state" in Python; the pickle file will
+    # contain all we need if we want to run the analysis, plotting,
+    # etc. later/again.
+    if pickle is not None:
+        print "Pickling results to file %s" % pickle
+        dopickle(pickle,(data,sig,tracesdict,chi2red_trace))
+
+
+    # return these for the plotting routine
+    vars = ['rin','rout','Mion']  # CDS
+    xlabels = [r'inner radius (pc)',r'outer radius (pc)',r'M$_{\rm ion}$ (M$_{\odot}$)']  #CDS
+    
+    return data, data2d, sig, sig2d, model, M, tracesdict, chi2red_trace, vars, xlabels, modeltype
+
+
+def fit_S61_TNS_continuous(datafile='/home/robert/science/ownpapers/lbv-paper/data-em-maps/em_map_s61-8GHz.fits',\
+                           sigmafile='/home/robert/science/ownpapers/lbv-paper/data-em-maps/err_em_map_s61-8GHz.fits',\
+                           nsample=500,nburn=100,physical_units=True,return_Mion=True,pickle=None,modeltype='TruncatedNormalShell'):
+
+    # get image, flatten, normalize
+    print "Reading data file"
+    data = Data(fitsfile=datafile)
+    data2d = data.data2d.copy()
+    print "data.npix = ", data.npix
+
+    # get uncertainties images, flatten, normalize same as image
+    print "Reading uncertainties file"
+    sig = Data(sigmafile)
+    sig2d = sig.data2d.copy()  # CAUTION!
+
+    co = (data.data2d.mask == False)
+    nmeas = (data.data2d.mask[co]).size  # number of non-masked pixels = number of data points
+    
+    # model
+    print "Defining model"
+    npix = data.npix
+    npix2 = npix/2
+    px = 2/float(npix)
+
+    # factor to translate from parsecs to pixels on cube with [-1,1] ranges
+    factor = (data.px2pc*data.npix/2.)
+             
+    # ///// SET UP PRIORS
+    eps = 0.001  # to avoid numerical armageddons
+    r = pymc.Uniform('r',eps/factor,0.4/factor)  # r=mu of the Gaussian; values are in parsec, factor converts them to unit hypercube
+    width = pymc.Uniform('width',eps/factor,(0.4/factor))#,value=0.05)  # width=sigma of the Gaussian
+    clipa = pymc.Uniform('clipa',eps,r-eps)
     clipb = pymc.Uniform('clipb',r+eps,0.7/factor)
     # offsets drawn from narrow Gaussian, truncated at [-2,+2] pixels from (x,y)=(0,0)
     xoff = pymc.TruncatedNormal('xoff',0,1./0.05**2,-2*px,2*px)
@@ -610,7 +809,11 @@ def fit_S61_TNS_continuous(datafile='/home/robert/science/ownpapers/lbv-paper/da
         dopickle(pickle,(data,sig,tracesdict,chi2red_trace))
 
     
-    return data, data2d, sig, sig2d, model, M, tracesdict, chi2red_trace
+    # return these for the plotting routine
+    vars = ['r','width','clipa','clipb','Mion']
+    xlabels = [r'radius (pc)',r'width (pc)',r'lower clip radius (pc)',r'upper clip radius (pc)',r'M$_{\rm ion}$ (M$_{\odot}$)']
+
+    return data, data2d, sig, sig2d, model, M, tracesdict, chi2red_trace, vars, xlabels, modeltype
 
 
 def logp_trace(model):
